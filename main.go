26a8a741b8af6b1ca309c7f227b31e8dbc9ace6f
RadKirill00 <radkirill00@mail.ru>
2024-06-10 14:11:42 +0300

    first step

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+    
+  	"github.com/gofiber/fiber/v2"
+	  "github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+
+type Todo struct {
+  ID        int     `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []Todo{
+  {ID:1, Name: "abc", Completed: false},
+  {ID: 2, Name: "def", Completed: true},
+}
+
+
 func main() {
-	
+  app := fiber.New()
+  app.Use(logger.New())
+
+  app.Get("/", func( c *fiber.Ctx) error {
+    return c.SendString("Hello world")
+  })
+
+  app.Listen(":5000")
 }
